884db5f86ff7952b891679f1f1f2551507f24122
Alvaroogaarcia <alvaro.g@opendeusto.es>
2026-04-22 13:15:59 +0200

    feat(dto): añadir campos eventoFecha, eventoHora y eventoUbicacion a TicketResponseDTO

--- src/main/java/com/ProyectoProcesosSoftware/dto/TicketResponseDTO.java
+++ src/main/java/com/ProyectoProcesosSoftware/dto/TicketResponseDTO.java
@@ -2,7 +2,9 @@ package com.ProyectoProcesosSoftware.dto;
 
 import lombok.Data;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import com.ProyectoProcesosSoftware.model.TicketStatus;
 
@@ -10,10 +12,19 @@ import com.ProyectoProcesosSoftware.model.TicketStatus;
 public class TicketResponseDTO {
     private Long id;
     private String uuid;
+
+    // Datos del evento
     private Long eventoId;
     private String eventoNombre;
+    private LocalDate eventoFecha;
+    private LocalTime eventoHora;
+    private String eventoUbicacion;
+
+    // Datos del asistente
     private Long asistenteId;
     private String asistenteNombre;
+
+    // Datos de la compra
     private BigDecimal precioFinal;
     private String estrategiaPrecio;
     private LocalDateTime fechaCompra;
